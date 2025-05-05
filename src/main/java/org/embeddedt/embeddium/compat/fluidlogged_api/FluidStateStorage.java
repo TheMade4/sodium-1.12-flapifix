52f9137327861b2644e444b1df628d5233400f1b
jbredwards <47703524+jbredwards@users.noreply.github.com>
2025-05-05 18:49:57 -0400

    Make FluidStateStorage clone data

--- src/main/java/org/embeddedt/embeddium/compat/fluidlogged_api/FluidStateStorage.java
+++ src/main/java/org/embeddedt/embeddium/compat/fluidlogged_api/FluidStateStorage.java
@@ -3,23 +3,31 @@ package org.embeddedt.embeddium.compat.fluidlogged_api;
 import git.jbredwards.fluidlogged_api.api.capability.IFluidStateCapability;
 import git.jbredwards.fluidlogged_api.api.capability.IFluidStateContainer;
 import git.jbredwards.fluidlogged_api.api.util.FluidState;
+import net.minecraft.world.chunk.BlockStateContainer;
 import net.minecraft.world.chunk.Chunk;
 
 import java.util.Objects;
 
 /**
- * Wrapper class for {@link IFluidStateContainer}.
+ * Holds a 16x16x16 copy of {@link IFluidStateContainer} data.
  */
 public class FluidStateStorage {
-    private final IFluidStateContainer container;
-    private final int containerY;
+    private final BlockStateContainer data = new BlockStateContainer();
 
-    public FluidStateStorage(Chunk chunk, int y) {
-        container = Objects.requireNonNull(IFluidStateCapability.get(chunk)).getContainer(y);
-        containerY = y;
+    public FluidStateStorage(Chunk chunkIn, int yIn) {
+        IFluidStateContainer container = Objects.requireNonNull(IFluidStateCapability.get(chunkIn)).getContainer(yIn);
+        container.forEach((pos, fluidState) -> {
+            int y = container.deserializeY(pos);
+            if (y >> 4 == yIn >> 4) {
+                int x = container.deserializeX(pos);
+                int z = container.deserializeZ(pos);
+
+                data.set(x & 15, y & 15, z & 15, fluidState.getState());
+            }
+        });
     }
 
     public Object get(int x, int y, int z) {
-        return container.getFluidState(x, containerY | y, z, FluidState.EMPTY);
+        return FluidState.of(data.get(x, y, z));
     }
 }
